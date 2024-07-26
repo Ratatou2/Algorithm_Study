143a1900a594fa2fbc62c59a1ac3c9cc705b111a
Ratatou2 <61686603+Ratatou2@users.noreply.github.com>
2024-07-26 09:14:26 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 84 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -15,7 +15,7 @@
 - 그냥 뭐 공백부터 찍어주고 그 뒤에 별 찍으면 되지
 
 [보완점]
-- 오 JAVA 11부터는 [String].repeat() 이란걸 쓸 수 있다 넘 신기
+- 오 JAVA 11부터는 [String].repeat() 이란걸 쓸 수 있다 넘 신기 (repeat이 4ms 더 빨랐음, 성능은 비슷한듯)
 - 그리고 나랑 똑같은 코드인데 72ms 나오는 사람은 뭐지..? JAVA 버전 차이인가? 함 해봐야지
 */
 
@@ -32,22 +32,22 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         // repeat ver (JAVA 11부터 지원)
-        for (int i = N; 0 < i; i--) {
-            sb.append(" ".repeat(Math.max(0, N - i)));  // 공백부터 추가
-            sb.append("*".repeat(i));  //
-            sb.append("\n");
-        }
-
-          // 반복문 ver
 //        for (int i = N; 0 < i; i--) {
-//            for (int j = 0; j < N-i; j++) {
-//                sb.append(" ");
-//            }
-//            for (int j = 0; j < i; j++) {
-//                sb.append("*");
-//            }
+//            sb.append(" ".repeat(Math.max(0, N - i)));  // 공백부터 추가
+//            sb.append("*".repeat(i));  //
 //            sb.append("\n");
 //        }
+
+        // 반복문 ver
+        for (int i = N; 0 < i; i--) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
 
         System.out.println(sb);
     }
