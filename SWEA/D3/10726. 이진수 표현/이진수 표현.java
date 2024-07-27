3e5e05065b3cf7e32c8c9c404f2103b132deef8f
Ratatou2 <61686603+Ratatou2@users.noreply.github.com>
2024-07-27 16:46:52 +0900

    [D3] Title: 이진수 표현, Time: 420 ms, Memory: 67,116 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -31,23 +31,35 @@ B형, 02, 이진수 표현
 [보완점]
 - 일단 직접 2진수로 만들어줄 필요도 없고(Integer.toBinaryString), 비트 마스킹이란걸 쓰면 편하게 할 수 있다고 함
 - 애초에 비트마스킹 처음 접했을 때, 어려워서 기피했던 거 같은데 이 기회에 짚고 넘어가는게 좋겠다
+- 초반에 틀렸던 것은 2진수를 만드는 과정에서 틀린 것이더라 (Integer.toBinaryString 하니까 틀린 것 없었음...)
+- 논리회로 때 배웠던 AND, OR, XOR, NOT 등을 이렇게 써먹다니 진짜 누가 개발했는지 천재들이 많은 것 같다...
 */
 public class Solution {
-    static boolean makeBinaryAndCheck(int N, int num) {
-        StringBuilder binary = new StringBuilder();
-        boolean result = false;
-
-        String[] temp = Integer.toBinaryString(num).split("");
-
-        // N개만큼 켜져 있는지 확인
-        for (int i = 0; i < N; i++) {
-            if (temp.length <= i) return false;
-            if (Integer.parseInt(temp[temp.length - i - 1]) == 1) result = true;
-            else return false;
-        }
-
-        return result;
+    // 비트마스킹 O
+    static boolean makeBinaryAndCheck(int N, int M) {
+        // 주의 해야하는 것이 있다.
+        // N 자릿수만큼만 1인지 확인하려는 것이면, 딱 N만큼만 이동하면 된다.
+        // 그리고 나서 1을 빼줘야 N-1 자리부터 전부 1이 된다
+        // 그 상태로 비교하려는 숫자 N과 OR 연산을 하면 숫자 M에서 N 자릿수가 전부 1인 숫자를 만들 수 있다
+        // 결과값이 M과 동일하다면, M은 N 자릿수가 모두 1을 만족한다는 의미이다 
+        return (M | ((1 << N) - 1)) == M;
     }
+
+    // 비트마스킹 X
+//    static boolean makeBinaryAndCheck(int N, int num) {
+//        boolean result = false;
+//
+//        String[] temp = Integer.toBinaryString(num).split("");
+//
+//        // N개만큼 켜져 있는지 확인
+//        for (int i = 0; i < N; i++) {
+//            if (temp.length <= i) return false;
+//            if (Integer.parseInt(temp[temp.length - i - 1]) == 1) result = true;
+//            else return false;
+//        }
+//
+//        return result;
+//    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
