1c06ea32dfab5ba58a07869610c7636db0c60eca
Ratatou2 <61686603+Ratatou2@users.noreply.github.com>
2024-08-03 13:11:59 +0900

    [Gold IV] Title: 중복 제거, Time: 1132 ms, Memory: 316520 KB -BaekjoonHub

--- 백준/Gold/13701. 중복 제거/중복 제거.java
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -21,7 +21,13 @@
 
 [보완점]
 - BitSet이란게 있단다. 심지어 배열 크기조차 정해줄 필요없다고... (뭐 이딴게 다있ㅇ..읍읍)
-
+- 비트 마스킹 풀이는 이해하는데 좀 어려웠다
+- 일단 int는 32bit이다 그러니까 int 배열을 만들어서 이걸 bitset처럼 쓰겠다는 의미 (대단한 발상...)
+- 32는 2^5니까 주어진 조건인 2^25를 해소하려면 int 배열은 2^20만큼 필요하게 되는것이고 그렇기에 int[] number = new int[1<<20];가 되는 것이다.
+- a / 32는 배열에서의 index를 의미
+- a % 32는 배열 index를 내부의 bit를 의미
+- 그렇게해서 이전에 등록된 적이 있으면 넘어가고, 아니면 등록하는 로직
+- 이런 아이디어는 대체 어떻게 생각해내는 것인지 참... 신기...
 */
 
 
@@ -33,18 +39,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         BitSet isExist = new BitSet();
 
-        int i = 1;
-        while (st.hasMoreTokens()) {
-            int number = Integer.parseInt(st.nextToken());
+//        while (st.hasMoreTokens()) {
+//            int number = Integer.parseInt(st.nextToken());
+//
+//            // 이전에 없었던 번호면 BitSet에 표기 + 답에 추가
+//            if (!isExist.get(number)) {
+//                isExist.set(number);
+//                sb.append(number).append(" ");
+//            }
+//        }
 
-            // 이전에 없었던 번호면 BitSet에 표기 + 답에 추가
-            if (!isExist.get(number)) {
-                isExist.set(number);
-                sb.append(number).append(" ");
+        // 비트마스크 풀이
+        int[] number = new int[1 << 20];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            int a = Integer.parseInt(st.nextToken());
+            int x = a / 32;
+            int y = a % 32;
+
+            // 해당 몫과 나머지가 기존에 있었는지 체크
+            if((number[x] & (1 << y)) != (1 << y)){
+                
+                // 해당 몫 위치에 나머지 원소 저장
+                number[x] |= (1 << y);
+                sb.append(a).append(" ");
             }
         }
 
