08b005fd7d37c684cf1d93693c5a365f192692be
Ratatou2 <61686603+Ratatou2@users.noreply.github.com>
2024-08-11 21:52:19 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 29508 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,60 @@
+/*
+[백준]
+2164, 카드2
+
+[문제파악]
+- N장의 카드가 있다.
+- 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
+
+- 이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다.
+- 우선, 제일 위에 있는 카드를 바닥에 버린다.
+- 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+- 예를 들어 N=4인 경우를 생각해 보자.
+- 카드는 제일 위에서부터 1234 의 순서로 놓여있다.
+- 1을 버리면 234가 남는다.
+- 여기서 2를 제일 아래로 옮기면 342가 된다.
+- 3을 버리면 42가 되고,
+- 4를 밑으로 옮기면 24가 된다.
+- 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
+
+N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
+
+[입력]
+- 첫째 줄에 정수 N(1 ≤ N ≤ 500,000)이 주어진다.
+
+[출력]
+- 첫째 줄에 남게 되는 카드의 번호를 출력한다.
+
+[구현방법]
+- ArrayDeque 쓴다음, 숫자 작은 것부터 위에 올라오도록 밀어넣으면 됨
+
+[보완점]
+
+*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> q = new ArrayDeque<Integer>();
+
+        // 숫자 밀어넣기
+        for (int i = 0; i < N; i++) q.push(N - i);
+
+        for (int i = 0; i < N; i++) {
+            if (q.size() == 1) break;  // 허나 남으면 정답이니 멈춤!
+            q.pop();
+            q.addLast(q.pop());
+        }
+
+        System.out.println(q.getFirst());
+    }
+}
