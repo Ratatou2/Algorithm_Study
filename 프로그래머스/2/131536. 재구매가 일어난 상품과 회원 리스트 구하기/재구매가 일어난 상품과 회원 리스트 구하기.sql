cb350e05abd406648323c4db0a1fafa12a7807bf
jinak_Samsung_laptop <ssafytou2@gmail.com>
2024-10-22 17:17:28 +0900

    241022 Having절 되짚기

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,3 +1,7 @@
+# Where과 Having의 차이를 잘 알아야 쉽게 풀 수 있다
+# Where은 그룹화 이전에, 각 행(row) 단위로 조건을 필터링할 때 사용
+# Having은 Group으로 묶은 다음에 결과에 대한 조건을 적용할 때 사용한다
+
 select user_id, product_id
     from ONLINE_SALE 
     group by user_id, product_id
