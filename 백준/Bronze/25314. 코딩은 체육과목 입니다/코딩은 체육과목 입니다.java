28ce02fd9e67dca68c0cb7fe8f24878dba1a1592
Jinak <ws423257@gmail.com>
2025-01-06 23:36:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 64 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -14,7 +14,8 @@
 - N을 4로 나눠서 그 갯수만큼 long을 작성하고 맨 마지막에 int를 붙여준다
 
 [보완점]
-
+- 문제를 잘 좀 읽자 N은 '항상' 4의 배수로 제공된다
+- 즉 삼항연산자를 써서 4의 배수인지를 체크하는 코드는 필요 없는 셈
 
 */
 
@@ -28,9 +29,9 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int repeat = 0 == N % 4 ? N/4 : N/4 + 1;
+        // int repeat = 0 == N % 4 ? N/4 : N/4 + 1;  // N은 항상 4의 배수로 주어지기 때문에 사실상 필요 없는 부분이다
 
-        for (int i = 0; i < repeat; i++) sb.append("long ");
+        for (int i = 0; i < N/4; i++) sb.append("long ");
         sb.append("int");
 
         System.out.println(sb);
