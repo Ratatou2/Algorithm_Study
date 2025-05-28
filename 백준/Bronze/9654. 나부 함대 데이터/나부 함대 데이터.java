e34fea6ed0ef80912e59b8df71e50ec8512b05b5
Jinak <ws423257@gmail.com>
2025-05-28 23:15:08 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 64 ms, Memory: 11328 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,27 @@
+/*
+[백준]
+9654, 나부 함대 데이터
+
+[문제파악]
+나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+
+[출력]
+나부 행성의 함대의 정보를 아래와 예제 출력과 같은 표로 출력한다.
+처음 두 열의 너비는 문자 15개, 세 번째 열은 11개, 마지막 열의 너비는 10개이다.
+
+[구현방법]
+
+
+[보완점]
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+                            "N2 Bomber      Heavy Fighter  Limited    21        \n" +
+                            "J-Type 327     Light Combat   Unlimited  1         \n" +
+                            "NX Cruiser     Medium Fighter Limited    18        \n" +
+                            "N1 Starfighter Medium Fighter Unlimited  25        \n" +
+                            "Royal Cruiser  Light Combat   Limited    4    ");
+    }
+}
