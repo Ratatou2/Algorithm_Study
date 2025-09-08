fa2e2c45ff6d3729b15acaea2f608a0b3621f3a7
Jinak <ws423257@gmail.com>
2025-09-08 23:40:15 +0900

    [Bronze V] Title: 오늘 날짜, Time: 96 ms, Memory: 13332 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,17 @@
+
+
+/*
+[백준]
+10699, 오늘 날짜
+
+[구현방법]
+- LocalDate.now() 쓰면 됨
+*/
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
