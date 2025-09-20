04fdd8dee57da49100c084adde6f5706baa306a7
Jinak <ws423257@gmail.com>
2025-09-20 17:50:22 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,34 @@
+
+
+/*
+[백준]
+24263, 알고리즘 수업 - 알고리즘의 수행 시간 2
+
+[문제파악]
+
+
+[입력]
+
+
+[출력]
+
+
+[구현방법]
+
+
+[보완점]
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println(N);  // 코드1 실행 횟수 = n
+        System.out.println(1);  // 최고차항 차수 = 1 (O(n))
+    }
+}
