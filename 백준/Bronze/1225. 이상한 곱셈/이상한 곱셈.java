127413e58039a188fef4c5c076c1a06897a0c575
Jinak <ws423257@gmail.com>
2025-09-24 21:06:41 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 232 ms, Memory: 207520 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,44 @@
+
+
+/*
+[백준]
+1225, 이상한 곱셈
+
+[문제파악]
+A×B를 계산하다 지겨워진 형택이는 A×B를 새로운 방법으로 정의하려고 한다.
+A에서 한 자리를 뽑고 × B에서 임의로 한 자리를 뽑아 곱한다.
+의 가능한 모든 조합 (A가 n자리, B가 m자리 수라면 총 가능한 조합은 n×m개)을 더한 수로 정의하려고 한다.
+예를 들어 121×34는
+1×3 + 1×4 + 2×3 + 2×4 + 1×3 + 1×4 = 28
+이 된다. 이러한 형택이의 곱셈 결과를 구하는 프로그램을 작성하시오.
+
+[입력]
+첫째 줄에 A와 B가 주어진다. 주어지는 두 수는 모두 10,000자리를 넘지 않는 음이 아닌 정수이다. 수가 0인 경우에는 0만 주어지며, 그 외의 경우 수는 0으로 시작하지 않는다.
+
+[출력]
+첫째 줄에 형택이의 곱셈 결과를 출력한다.
+
+[구현방법]
+
+
+[보완점]
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        long result = 0;
+        for (char a : input[0].toCharArray()) {
+            for (char b : input[1].toCharArray()) {
+                result += (long)(a - '0') * (long)(b -'0');
+            }
+        }
+
+        System.out.println(result);
+    }
+}
